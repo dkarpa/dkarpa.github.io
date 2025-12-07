92b2cf1f4d39b4ba3491fded667b15f9a3986e94
dkarpa <83760419+dkarpa@users.noreply.github.com>
2025-12-07 12:30:00 +0000

    Deploying to gh-pages from @ dkarpa/dkarpa.github.io@03b7351a406f0ec38a794d992266d5790b7ddcdb 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "dkarpa/dkarpa",
+    "data-repo": "dkarpa/dkarpa.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
